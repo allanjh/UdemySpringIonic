fd97360d1d17c6d31570cb41853f71e4a82955d2
Allan Jhonny Araújo Santos <allanjh@gmail.com>
2021-12-28 16:12:27 -0300

    criacação de subtotal e total de pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -90,6 +90,10 @@ public class ItemPedido implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	
 	
 	
